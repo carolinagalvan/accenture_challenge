b8c393e884b58ec00c92bd10334a9342d127b3f4
pedrosc97 <pedro.sc.97@gmail.com>
2019-10-30 04:46:18 -0600

    Added yolo-opencv python script for car detection

---
+++ car_detection/yolo_opencv.py
@@ -0,0 +1,124 @@
+#############################################
+# Object detection - YOLO - OpenCV
+# Author : Arun Ponnusamy   (July 16, 2018)
+# Website : http://www.arunponnusamy.com
+############################################
+
+import cv2
+import argparse
+import numpy as np
+import imutils
+import pytesseract
+from PIL import Image
+
+ap = argparse.ArgumentParser()
+#ap.add_argument('-i', '--image', required=True,
+#                help = 'path to input image')
+ap.add_argument('-c', '--config', required=True,
+                help = 'path to yolo config file')
+ap.add_argument('-w', '--weights', required=True,
+                help = 'path to yolo pre-trained weights')
+ap.add_argument('-cl', '--classes', required=True,
+                help = 'path to text file containing class names')
+args = ap.parse_args()
+
+
+def get_output_layers(net):
+    
+    layer_names = net.getLayerNames()
+    
+    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+    return output_layers
+
+
+def draw_prediction(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
+
+    label = str(classes[class_id])
+
+    color = COLORS[class_id]
+
+    cv2.rectangle(img, (x,y), (x_plus_w,y_plus_h), color, 2)
+
+    cv2.putText(img, label, (x-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+
+cap = cv2.VideoCapture('entrance1.mov')
+roi = np.zeros((430,430))
+
+if (cap.isOpened()== False): 
+    print("Error opening video stream or file")
+
+classes = None
+
+with open(args.classes, 'r') as f:
+    classes = [line.strip() for line in f.readlines()]
+
+COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
+net = cv2.dnn.readNet(args.weights, args.config)
+
+carArea = 0
+lifeCounter = 0
+
+while (1):
+
+    ret, image = cap.read()
+
+    Width = image.shape[1]
+    Height = image.shape[0]
+    scale = 0.00392
+   
+    blob = cv2.dnn.blobFromImage(image, scale, (416,416), (0,0,0), True, crop=False)
+
+    net.setInput(blob)
+
+    outs = net.forward(get_output_layers(net))
+
+    class_ids = []
+    confidences = []
+    boxes = []
+    conf_threshold = 0.5
+    nms_threshold = 0.4
+
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                center_x = int(detection[0] * Width)
+                center_y = int(detection[1] * Height)
+                w = int(detection[2] * Width)
+                h = int(detection[3] * Height)
+                x = center_x - w / 2
+                y = center_y - h / 2
+                class_ids.append(class_id)
+                confidences.append(float(confidence))
+                boxes.append([x, y, w, h])
+
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+
+    for i in indices:
+        i = i[0]
+        if class_ids[i] == 2:
+            box = boxes[i]
+            x = box[0]
+            y = box[1]
+            w = box[2]
+            h = box[3]
+            draw_prediction(image, class_ids[i], confidences[i], round(x), round(y), round(x+w), round(y+h))
+            roi = image[round(y):round(y+h), round(x):round(x+w)]
+            carArea = roi.shape[0] * roi.shape[1]
+           
+    cv2.imshow("object detection", image)
+    
+    if carArea >= 48000:
+        lifeCounter += 1
+        if lifeCounter == 3:
+            cv2.imshow('roi', roi)
+    
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break 
+        
+cap.release() 
+cv2.destroyAllWindows()
