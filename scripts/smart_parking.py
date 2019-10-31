d392acb909e7337fb5899692cc6275c18c71b76f
Clarissa Miranda <clarissamiranda@MacBook-Pro-de-C.local>
2019-10-31 15:31:31 -0600

    aws facade

--- scripts/smart_parking.py
+++ scripts/smart_parking.py
@@ -1,7 +1,9 @@
 from car_detector import car_detector
 from face_detector import face_detector
+from aws_serv import aws_serv
 import argparse
 import cv2
+import os
 
 ap = argparse.ArgumentParser()
 
@@ -18,11 +20,20 @@ args = ap.parse_args()
 carDetector = car_detector(args.config, args.weights, args.classes)
 faceDetector = face_detector('../user_database/userdatabase_1.csv', 4, '../user_database/', '.jpeg')
 
-car_roi = carDetector.wait_for_car(args.source)
-cv2.imshow('ROI', car_roi)
-cv2.waitKey()
-cv2.destroyAllWindows()
 user_name, user_face = faceDetector.wait_for_face(0)
-cv2.imshow('User', user_face)
-cv2.waitKey()
-cv2.destroyAllWindows()
+car_roi = carDetector.wait_for_car(args.source)
+
+bucket_name = os.getenv('BUCKET_NAME')
+access_id = os.getenv('ACCESS_ID')
+secret_key = os.getenv('SECRET_KEY')
+
+# Recognize plate text
+image_binary = aws_serv.img_to_byte(car_roi)
+rek_client = aws_serv.new_rekclient(access_id, secret_key)
+objectName = aws_serv.text_picture(rek_client, image_binary)
+
+# Upload image to bucket
+s3_client = aws_serv.new_s3client(access_id, secret_key)
+success = aws_serv.new_object(s3_client, objectName, image_binary, bucket_name)
+if success:
+	print(f'Added {bucketName} to {objectName}')
